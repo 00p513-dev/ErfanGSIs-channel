9989b9fb16fa3959600256ca611080176f4d4457
Amy <amy@turbox.uk>
2021-05-18 13:15:14 +0100

    JoyUI: Add device patches

--- roms/10/JoyUI/make.sh
+++ roms/10/JoyUI/make.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 systempath=$1
+LOCALDIR=$2
+device=$3
 thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
 # Add BlackShark things
@@ -24,3 +26,14 @@ rm -rf $1/priv-app/dpmserviceapp/
 # Custom Manifest   
 python $thispath/../../../scripts/custom_manifest.py $thispath/../../../tmp/manifest.xml $thispath/manifest.xml $1/etc/vintf/manifest.xml
 cp -fpr $thispath/../../../tmp/manifest.xml $1/etc/vintf/manifest.xml
+
+# Device specific patches
+
+if [ "$device" == "channel" ]; then
+    sed -i 's/persist.miui.density_v2=440/persist.miui.density_v2=274/' $1/build.prop
+    sed -i 's/ro.sf.lcd_density=440/ro.sf.lcd_density=274/' $1/build.prop
+    echo "" >> $1/build.prop
+    echo "# OpenGL version" >> $1/build.prop
+    echo "ro.opengles.version=196610" >> $1/build.prop
+    cp $LOCALDIR/devices/channel/MIUI/channel.xml $1/etc/device_features/channel.xml
+fi
